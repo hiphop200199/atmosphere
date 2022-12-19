8772436e284dd5f327e4fab3cc7fd565a2ce066b
Eric Tsai <hiphop200199@gmail.com>
2022-12-19 21:50:29 +0800

    嘗試增加用js更換背景的code

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let bubbles = document.querySelectorAll(".bubble");
+  let container=document.querySelector(".web-container");
   let aurora = document.getElementById("aurora");
   let snowman = document.getElementById("snowman");
   let flowerRing = document.getElementById("flowerRing");
   let dam = document.getElementById("dam");
-  let elem = document.querySelector("p");
-  elem.addEventListener("click", changeColor);
-  function changeColor() {
-    elem.style.color = `rgb(${Math.floor(Math.random() * 255)} ,${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-    elem.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)} ,${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-  };
+  let title=document.getElementById("web-title");
+  dam.addEventListener("click",function(){
+    container.classList.add("dam");
+    container.classList.remove("aurora");
+    container.classList.remove("snowman");
+    container.classList.remove("flower-ring");
+    title.classList.add("dam");
+    title.classList.remove("aurora");
+    title.classList.remove("snowman");
+    title.classList.remove("flower-ring");
+
+  })
+  aurora.addEventListener("click",function(){
+    container.classList.add("aurora");
+    container.classList.remove("dam");
+    container.classList.remove("snowman");
+    container.classList.remove("flower-ring");
+    title.classList.add("aurora");
+    title.classList.remove("dam");
+    title.classList.remove("snowman");
+    title.classList.remove("flower-ring");
+
+  })
+  snowman.addEventListener("click",function(){
+    container.classList.add("snowman");
+    container.classList.remove("aurora");
+    container.classList.remove("dam");
+    container.classList.remove("flower-ring");
+    title.classList.add("snowman");
+    title.classList.remove("aurora");
+    title.classList.remove("dam");
+    title.classList.remove("flower-ring");
+
+  })
+  flowerRing.addEventListener("click",function(){
+    container.classList.add("flower-ring");
+    container.classList.remove("aurora");
+    container.classList.remove("snowman");
+    container.classList.remove("dam");
+    title.classList.add("flower-ring");
+    title.classList.remove("aurora");
+    title.classList.remove("snowman");
+    title.classList.remove("dam");
+
+  })
+  
+  
+ 
 
 
-  for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].addEventListener("click", changePosition);
-    function changePosition() {
-      bubbles[i].style.top = Math.floor(Math.random() * 1000) + 'px';
-      bubbles[i].style.left = Math.floor(Math.random() * 1000) + 'px';
-    }
-  };
+ 
+ 
 });/*等網站的DOM都載入完後再執行*/
 
 
